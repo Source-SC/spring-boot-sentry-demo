7343116a48015b00b71d99abe5a54ce93b181b39
Source-SC <kimdh425@naver.com>
2022-06-14 16:39:26 +0900

    init

---
+++ src/main/java/com/example/springbootsentrydemo/SpringBootSentryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsentrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSentryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSentryDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootsentrydemo/SpringBootSentryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsentrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSentryDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
