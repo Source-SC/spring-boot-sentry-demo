28fa52000fba18c70a6015e89007e0d971dc3ced
Source-SC <kimdh425@naver.com>
2022-06-30 11:12:24 +0900

    bugsnag 설정 적용

--- src/main/java/com/example/springbootsentrydemo/controller/MyController.java
+++ src/main/java/com/example/springbootsentrydemo/controller/MyController.java
@@ -1,20 +1,31 @@
 package com.example.springbootsentrydemo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bugsnag.Bugsnag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.lang.Exception;
 import io.sentry.Sentry;
 
 @RestController
 public class MyController {
 
+    @Autowired
+    private Bugsnag bugsnag;
+
     @GetMapping("error1")
     public String error1(){
+        bugsnag.notify(new RuntimeException("My TEST 0630"));
         try {
             throw new Exception("This is a test.");
         } catch (Exception e) {
             Sentry.captureException(e);
         }
         return "error1";
+    }
+
+    @PostMapping("make/error/{errorId}")
+    public void makeError(@PathVariable String errorId){
+        bugsnag.notify(new RuntimeException(errorId));
     }
 }
